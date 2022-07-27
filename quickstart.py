afff1ec08af4c8013c6377dd1907ee53a020526c
trnciii <doityan223@gmail.com>
2022-07-28 08:46:10 +0900

    split into functions

--- quickstart.py
+++ quickstart.py
@@ -13,7 +13,7 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
 
-def main():
+def auth():
 	"""Shows basic usage of the Drive v3 API.
 	Prints the names and ids of the first 10 files the user has access to.
 	"""
@@ -35,24 +35,32 @@ def main():
 		with open('token.json', 'w') as token:
 			token.write(creds.to_json())
 
-	try:
-		service = build('drive', 'v3', credentials=creds)
+	return creds
 
-		# Call the Drive v3 API
-		results = service.files().list(pageSize=10).execute()
-		items = results.get('files', [])
 
-		if not items:
-			print('No files found.')
-			return
-		print('Files:')
-		for item in items:
-			print(item['name'])
-			pprint.pprint({k: v for k, v in item.items() if k != 'name'})
-			print()
-			# print(u'{0} ({1})'.format(item['name'], item['id']))
+def list_items(service):
+	# Call the Drive v3 API
+	results = service.files().list(pageSize=10).execute()
+	items = results.get('files', [])
+
+	if not items:
+		print('No files found.')
+		return
+	print('Files:')
+	for item in items:
+		print(item['name'])
+		pprint.pprint({k: v for k, v in item.items() if k != 'name'})
+		print()
+		# print(u'{0} ({1})'.format(item['name'], item['id']))
 
 		print('keys', item.keys())
+
+
+def main():
+	try:
+		service = build('drive', 'v3', credentials=auth())
+		list_items(service)
+
 	except HttpError as error:
 		# TODO(developer) - Handle errors from drive API.
 		print(f'An error occurred: {error}')
